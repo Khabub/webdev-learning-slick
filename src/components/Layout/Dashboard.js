b6d19f88857c42b580d9578991cecff0b6ef8c62
Robert Rozehnal <rob.kab.roz@gmail.com>
2022-06-22 16:56:31 +0200

    dashboard position

--- src/components/Layout/Dashboard.js
+++ src/components/Layout/Dashboard.js
@@ -9,7 +9,6 @@ import {
   YAxis,
   Tooltip,
   Legend,
-  ResponsiveContainer
 } from "recharts";
 
 const data = [
@@ -56,55 +55,66 @@ const Dashboard = () => {
       <div className={classes.dashLeft}>
         <h2>PRO acount</h2>
         <ul>
-          <li>Home Screen</li>
+          <li>
+            <a href="#test">Home Screen</a>
+          </li>
           <li>Dashboard</li>
           <li>Settings</li>
         </ul>
       </div>
       <div className={classes.dashboard}>
-        <ResponsiveContainer width={600} height="50%">
-      <AreaChart        
-        data={data}
-        margin={{ top: 10, left: 10, bottom: 10, right: 10 }}
-      >
-        <Area
-          type="monotone"
-          dataKey="quests"
-          stackId="0"
-          stroke="#0055ff"
-          fill="#0055ff"
-          dot={true}
-          name="Guild quests"
-        />
-        <Area
-          type="monotone"
-          dataKey="questsOther"
-          stackId="1"
-          stroke="#aaa"
-          fill="#aaa"
-          dot={true}
-          name="Guild quests"
-        />
-        <CartesianGrid stroke="#888" vertical={false} />
-        <XAxis dataKey="name" stroke="#fff" tickMargin={20} />
-        <YAxis
-          label={{
-            value: "Number of quests",
-            angle: -90,
-            position: "insideBottomLeft",
-            fill: "#fff",
-          }}
-          tickMargin={10}
-          stroke="#fff"
-        />
-        <Tooltip offset={20} stroke="#000" />
-        <Legend layout="horizontal" align="right" wrapperStyle={{ top: 300 }}
-        iconType="square" />
-      </AreaChart>
-      </ResponsiveContainer>
+       
+        <AreaChart   
+          width={200}
+          height={140}      
+          data={data}
+          margin={{ top: 10, left: 10, bottom: 10, right: 10 }}
+        >
+          <Area
+            type="monotone"
+            dataKey="quests"
+            stackId="0"
+            stroke="#0055ff"
+            fill="#0055ff"
+            dot={true}
+            name="Guild quests"            
+          />
+          <Area
+            type="monotone"
+            dataKey="questsOther"
+            stackId="1"
+            stroke="#aaa"
+            fill="#aaa"
+            dot={true}
+            name="Guild quests"
+          />
+          <CartesianGrid stroke="#888" vertical={false} />
+          <XAxis hide={true} />
+          <YAxis hide={true} />
+          <Tooltip offset={20} stroke="#000" />
+          <Legend
+            layout="horizontal"
+            align="right"
+            wrapperStyle={{ top: 135, fontSize: 8 }}
+            iconType="square"
+            iconSize={8}                    
+          />
+        </AreaChart>
+        
       </div>
     </div>
   );
 };
 
 export default Dashboard;
+
+// <XAxis dataKey="name" stroke="#fff" tickMargin={10} />
+// <YAxis
+//   label={{
+//     angle: -90,
+//     position: "insideBottomLeft",
+//     fill: "#fff",
+//   }}
+//   tickMargin={10}
+//   stroke="#fff"
+// />
